1a75c9237e4c79796bb72fa205220c21289a1adf
zhouzun <zhouzun@qq.com>
2024-08-16 17:32:03 +0800

    search

--- google/google_search.py
+++ google/google_search.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 import requests
 import re
@@ -14,29 +15,53 @@ import tool.encry
 # 配置
 API_KEY = 'AIzaSyDVGWYSuDRMX3GTM6NxqAxX7AxW4vq8qNE'
 SEARCH_ENGINE_ID = '45f1e3f35c4214993'
+# 设置排除的国家和地区
+EXCLUDED_COUNTRIES = ['中国', '马来西亚', '越南', '泰国']
+def run():
+    #排除地区国家：中国、马来西亚，越南，泰国、印度
+    query = (
+        'disposable gloves contact email phone '
+        '-site:.cn -site:.my -site:.vn -site:.th -site:.in -site:tw'
+    )
+    num = 10  # 限制数量10条
+    results = get_search_results(query, num)
+
+    for result in results:
+        title = result.get('title')
+        snippet = result.get('snippet')
+        link = result.get('link')
+        # 提取联系信息
+        emails, phones = extract_contact_info(snippet)
+        print(f'Title: {title}')
+        print(f'Link: {link}')
+        print(f'Emails: {", ".join(emails)}')
+        print(f'Phones: {", ".join(phones)}')
+        print('-' * 40)
+        save_to_database(query, link, ",".join(emails), ",".join(phones), 2)
 
 
 
-
-def get_search_results(query, num):
+def get_search_results(query, num,gl="us",lr=""):
     # 设置请求参数
     params = {
         'q': query,
         'num': num,
         'cx': SEARCH_ENGINE_ID,  # 你的Custom Search Engine ID
         'key': API_KEY,  # 你的API密钥
-        'gl': 'us',  # 谷歌区域代码，可以根据需要更改
-        # 'lr': 'lang_zh-CN',  # 搜索结果语言
-        'exclude': 'country:CN|country:MY|country:VN|country:TH|country:IN'  # 排除特定国家
+        'gl': gl,  # 最终用户的地理位置，可以根据需要更改
+        'lr': lr # 搜索结果语言
+
     }
 
     # 调用API
     response = requests.get('https://www.googleapis.com/customsearch/v1', params=params)
 
-
     # url = f'https://www.googleapis.com/customsearch/v1?q={query}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&num={num}'
     # response = requests.get(url)
     if response.status_code == 200:
+        # 将响应数据写入文件
+        with open('response.json', 'w', encoding='utf-8') as f:
+            json.dump(response.json(), f, ensure_ascii=False, indent=4)
         return response.json().get('items', [])
     else:
         print(f"Error: {response.status_code}")
@@ -71,24 +96,5 @@ def save_to_database(keyword,url, email, phone, category):
         print(f"数据库错误: {e}")
     finally:
         connection.disconnect()
-def run():
-    query = 'disposable gloves contact email phone'
-    num = 10  # 限制数量10条
-    results = get_search_results(query, num)
 
-    for result in results:
-        title = result.get('title')
-        snippet = result.get('snippet')
-        link = result.get('link')
-
-        # 提取联系信息
-        emails, phones = extract_contact_info(snippet)
-
-
-        print(f'Title: {title}')
-        print(f'Link: {link}')
-        print(f'Emails: {", ".join(emails)}')
-        print(f'Phones: {", ".join(phones)}')
-        print('-' * 40)
-        save_to_database(query, link, ",".join(emails), ",".join(phones), 2)
 
